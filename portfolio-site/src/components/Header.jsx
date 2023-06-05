a4e4aba933b6053a31f2cec3cb674410fbc2a00c
Morton Nicolaysen <mort1995@hotmail.com>
2023-06-05 23:45:26 +0100

    edit to navbar and header

--- portfolio-site/src/components/Header.jsx
+++ portfolio-site/src/components/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.css";
 export default function Header() {
   return (
     <div className="container">
-      <img className="profile-pic" src="./src/images/db099441-213f-46a9-81c0-97d76adb2c6f.JPG" alt="profile-pic" />
+      <img className="profile-pic" src="./src/components/images/db099441-213f-46a9-81c0-97d76adb2c6f.JPG" alt="profile-pic" />
       <h1 className="heading">Morton Nicolaysen</h1>
       <h3 className="subheading">Web Developer</h3>
     </div>
