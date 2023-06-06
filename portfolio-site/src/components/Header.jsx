d953b1b3539e4c39dd8a1acc8c8312f5285d8e8b
Morton Nicolaysen <mort1995@hotmail.com>
2023-06-06 09:42:39 +0100

    tried importing image first then implimenting it

--- portfolio-site/src/components/Header.jsx
+++ portfolio-site/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import "./Header.css";
+import profilePic from "/src/components/images/db099441-213f-46a9-81c0-97d76adb2c6f.JPG"
 
 export default function Header() {
   return (
     <div className="container">
-      <img className="profile-pic" src="/src/components/db099441-213f-46a9-81c0-97d76adb2c6f.JPG" alt="profile-pic" />
+      <img className="profile-pic" src={profilePic} alt="profile-pic" />
       <h1 className="heading">Morton Nicolaysen</h1>
       <h3 className="subheading">Web Developer</h3>
       <p>Lorem, ipsum dolor sit ametiis aliquid molestiae odit obcaecati sint?</p>
