ef3a75e44aa0c93fef440ab99cef169336f6813e
Morton Nicolaysen <mort1995@hotmail.com>
2023-06-06 11:27:18 +0100

    created routes

--- portfolio-site/src/App.jsx
+++ portfolio-site/src/App.jsx
@@ -3,14 +3,17 @@ import Navbar from "./components/Navbar"
 import About from "./components/About"
 import Skills from "./components/Skills"
 import './App.css'
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<Header />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/skills" element={<Skills/>}/>
+      </Routes>
       <Navbar />
-      <Header />
-      <About />
-      <Skills />
     </div>
   )
 }
--- portfolio-site/src/components/Navbar.jsx
+++ portfolio-site/src/components/Navbar.jsx
@@ -1,25 +1,37 @@
 import './Navbar.css';
+import { Link, useMatch, useLocation } from "react-router-dom";
+import PropTypes from "prop-types";
 
 export default function Navbar() {
   return (
-    <div className="navbar">
+    <nav className="navbar">
       <div className="navbar-container">
-        <button className="navbar-btn">
-        <i className="fa-solid fa-house"></i>
-        </button>
-        <button className="navbar-btn">
-          <i className="fa-solid fa-address-card"></i>
-        </button>
-        <button className="navbar-btn">
-        <i className="fa-solid fa-gear"></i>
-        </button>
-        <button className="navbar-btn">
-          <i className="fa-solid fa-diagram-project"></i>
-        </button>
-        <button className="navbar-btn">
-          <i className="fa-solid fa-envelope"></i>
-        </button>
+        <ul>
+          <CustomLink to="/" className="navbar-btn"><i className="fa-solid fa-house"></i></CustomLink>
+          <CustomLink to="/about" className="navbar-btn"><i className="fa-solid fa-address-card"></i></CustomLink>
+          <CustomLink to="/skills" className="navbar-btn"><i className="fa-solid fa-gear"></i></CustomLink>
+          <CustomLink to="/projects" className="navbar-btn"><i className="fa-solid fa-diagram-project"></i></CustomLink>
+          <CustomLink to="/contact" className="navbar-btn"><i className="fa-solid fa-envelope"></i></CustomLink>
+        </ul>
       </div>
-    </div>
+    </nav>
   );
 }
+
+function CustomLink({ to, children, ...props }) {
+  const { pathname } = useLocation();
+  const isActive = useMatch(to)?.pathname === pathname;
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
+}
+
+CustomLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
