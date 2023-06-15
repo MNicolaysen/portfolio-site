11b60adea2103d8eef3a166b4b02118bb5061051
Morton Nicolaysen <mort1995@hotmail.com>
2023-06-15 14:26:29 +0100

    removed map

--- portfolio-site/src/components/Contact.jsx
+++ portfolio-site/src/components/Contact.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 import ContactLinks from '../components/ContactLinks'
 import './Contact.css';
 
@@ -22,7 +21,6 @@ function EmailForm({ onSubmit, message, setMessage }) {
 
 function Contact() {
   const [message, setMessage] = useState('');
-  const [infoWindowOpen, setInfoWindowOpen] = useState(false);
   const [isSmallScreen, setIsSmallScreen] = useState(false);
 
   useEffect(() => {
@@ -47,46 +45,11 @@ function Contact() {
     window.location.href = mailtoLink;
   };
 
-  const mapContainerStyle = {
-    height: '300px',
-    width: '80%',
-  };
-
-  const center = {
-    lat: 51.55005,
-    lng: -0.14899,
-  };
-
-  const handleMarkerClick = () => {
-    setInfoWindowOpen(true);
-  };
-
-  const handleInfoWindowClose = () => {
-    setInfoWindowOpen(false);
-  };
-
   return (
     <div className='contact'>
       <h3 className='main-header'>Contact Me</h3>
       <EmailForm onSubmit={handleSubmit} message={message} setMessage={setMessage} />
       {isSmallScreen && <ContactLinks />}
-
-      <div className='map'>
-        <LoadScript googleMapsApiKey="AIzaSyA30ZDq_zS1GIMotMsdyP1eQhXqjbgtbcM">
-          <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={10}>
-            <Marker position={center} onClick={handleMarkerClick} />
-
-            {infoWindowOpen && (
-              <InfoWindow position={center} onCloseClick={handleInfoWindowClose}>
-                <div>
-                  <h4>Marker Info</h4>
-                  <p>Additional information about the marker.</p>
-                </div>
-              </InfoWindow>
-            )}
-          </GoogleMap>
-        </LoadScript>
-      </div>
     </div>
   );
 }
