0da523523f5b0161d5943689f6828c7f41024624
Morton Nicolaysen <mort1995@hotmail.com>
2023-06-15 14:35:38 +0100

    icons open in new tab

--- portfolio-site/src/components/ContactLinks.jsx
+++ portfolio-site/src/components/ContactLinks.jsx
@@ -2,22 +2,22 @@ export default function ContactLinks() {
   return (
     <ul className="contact-links">
       <li className="contact-link-header">
-        <a style={{ color: 'rgb(11,102,194)' }} href="https://www.linkedin.com/in/morton-nicolaysen/">
+        <a style={{ color: 'rgb(11,102,194)' }} href="https://www.linkedin.com/in/morton-nicolaysen/" target="_blank" rel="noopener noreferrer">
           <i className="fa-brands fa-linkedin"></i>
         </a>
       </li>
       <li className="contact-link-header">
-        <a style={{ color: 'black' }} href="https://github.com/MNicolaysen">
+        <a style={{ color: 'black' }} href="https://github.com/MNicolaysen" target="_blank" rel="noopener noreferrer">
           <i className="fa-brands fa-github"></i>
         </a>
       </li>
       <li className="contact-link-header">
-        <a className='facebook-icon' href="https://m.me/nicolaysen1995">
+        <a className='facebook-icon' href="https://m.me/nicolaysen1995" target="_blank" rel="noopener noreferrer">
           <i className="fa-brands fa-facebook-messenger"></i>
         </a>
       </li>
       <li className="contact-link-header">
-        <a className='instagram-icon' href="https://www.instagram.com/morton_nicolaysen/">
+        <a className='instagram-icon' href="https://www.instagram.com/morton_nicolaysen/" target="_blank" rel="noopener noreferrer">
           <i className="fa-brands fa-instagram"></i>
         </a>
       </li>
