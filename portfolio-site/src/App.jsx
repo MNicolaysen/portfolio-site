c771204cac95f245497adaa51bf0621d96746dd5
Morton Nicolaysen <mort1995@hotmail.com>
2023-06-20 00:04:20 +0100

    added mobile styling

--- portfolio-site/src/App.jsx
+++ portfolio-site/src/App.jsx
@@ -1,6 +1,4 @@
-import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
@@ -8,22 +6,8 @@ import Contact from "./components/Contact";
 import './App.css';
 
 function App() {
-  const isSmallScreen = window.matchMedia("(max-width: 1000px)").matches;
-
   return (
     <div className="app">
-      {isSmallScreen ? (
-        <div>
-          <Routes>
-            <Route path="/" element={<Header />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/experience" element={<Experience />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-          <Navbar />
-        </div>
-      ) : (
         <div className="container">
           <div className="left-components">
           <Header />
@@ -35,7 +19,6 @@ function App() {
             <Experience />
           </div>
         </div>
-      )}
     </div>
   );
 }
